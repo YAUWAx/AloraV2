908235e814caf54786e9d7602706220208711445
xYAZUWA <92874892+xYAZUWA@users.noreply.github.com>
2021-12-05 17:55:42 +0530

    Update MenuButton.js

--- plugins/MenuButton.js
+++ plugins/MenuButton.js
@@ -1,10 +1,1 @@
-const Asena = require('../events');
-const Config = require('../config');
-const {MessageType} = require('@adiwajshing/baileys');
 
-const Language = require('../language');
-const Lang = Language.getString('_asena');
-
-if (Config.WORKTYPE == 'private') {
-
-    Asena.addCommand({pattern: 'asena ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
